853b634249bc6c29a22518a953bf19bdb7bc4af9
Sara <125907191+sbili@users.noreply.github.com>
2024-11-17 08:40:27 -0800

    style nav

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,5 +1,5 @@
+import { css } from '@emotion/css';
 import { Link } from '@tanstack/react-router';
-import { CSSProperties } from 'react';
 import { BiFoodMenu } from 'react-icons/bi';
 import { FaHome } from 'react-icons/fa';
 import { GiCartwheel, GiNoodles } from 'react-icons/gi';
@@ -8,13 +8,17 @@ import { MdOutlineDinnerDining } from 'react-icons/md';
 import { PiBreadBold } from 'react-icons/pi';
 import { TbCake, TbSoup } from 'react-icons/tb';
 
-const linkStyle: CSSProperties = {
+const linkStyle = css({
   display: 'flex',
-  alignItems: 'center',
+  alignitems: 'center',
   gap: 5,
-  textDecoration: 'none',
+  textdecoration: 'none',
   color: 'black',
-};
+  padding: 5,
+  '&:hover': {
+    backgroundColor: 'whitesmoke',
+  },
+});
 
 export const Navbar = () => {
   return (
@@ -22,63 +26,63 @@ export const Navbar = () => {
       <Link
         to="/SaraCookbook"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <FaHome size="1.5rem" /> Home
       </Link>
       <Link
         to="/SaraCookbook/sandwich"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <PiBreadBold size="1.5rem" /> Sandwiches
       </Link>
       <Link
         to="/SaraCookbook/soup"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <TbSoup size="1.5rem" /> Soup
       </Link>
       <Link
         to="/SaraCookbook/entree"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <MdOutlineDinnerDining size="1.5rem" /> Entrees
       </Link>
       <Link
         to="/SaraCookbook/pasta"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <GiNoodles size="1.5rem" /> Pasta/Noodles
       </Link>
       <Link
         to="/SaraCookbook/dessert"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <TbCake size="1.5rem" /> Dessert
       </Link>
       <Link
         to="/SaraCookbook/drinks"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <LiaCocktailSolid size="1.5rem" /> Drinks
       </Link>
       <Link
         to="/SaraCookbook/cuisine"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <BiFoodMenu size="1.5rem" /> By Cuisine
       </Link>
       <Link
         to="/SaraCookbook/wheel"
         activeOptions={{ exact: true }}
-        style={linkStyle}
+        className={linkStyle}
       >
         <GiCartwheel size="1.5rem" /> The Wheel
       </Link>
