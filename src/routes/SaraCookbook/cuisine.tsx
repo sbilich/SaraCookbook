e6cccca975f34967bdacd1f5514cc09bfdeecf77
Sara <125907191+sbili@users.noreply.github.com>
2024-11-18 21:28:57 -0800

    update cuisine

--- src/routes/SaraCookbook/cuisine.tsx
+++ src/routes/SaraCookbook/cuisine.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/SaraCookbook/cuisine')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return 'Hello /cuisine!'
+  return 'Under construction';
 }
