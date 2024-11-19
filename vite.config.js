8c3e90bfe8a5caabdf30f5ec0de18f4f0b466894
Sara <125907191+sbili@users.noreply.github.com>
2024-11-18 20:56:43 -0800

    try to deploy 404

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,27 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { join, parse, resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint(), TanStackRouterVite()],
   base: '/SaraCookbook/',
+  build: {
+    rollupOptions: {
+      input: entryPoints('index.html', '404.html'),
+    },
+  },
 });
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map((entry) => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+
+  const config = Object.fromEntries(entries);
+  return config;
+}
