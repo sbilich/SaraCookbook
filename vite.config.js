df11d27317e8b88e2859d64ee35c4534825135a6
Sara <125907191+sbili@users.noreply.github.com>
2024-11-18 20:01:44 -0800

    Update base

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint(), TanStackRouterVite()],
-  base: '/SaraCookbook/',
+  base: '/',
 });
