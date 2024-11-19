8a0ecab1b098c264e32742346cd24c2bedcb7571
Sara <125907191+sbili@users.noreply.github.com>
2024-11-19 10:38:48 -0800

    add lemon pasta

--- src/recipes/recipe-data/recipes.ts
+++ src/recipes/recipe-data/recipes.ts
@@ -1,4 +1,5 @@
 import TomatoSoup from './tomato-soup.json';
 import PadThai from './pad-thai.json';
+import LemonZucchiniPasta from './lemon-zucchini-pasta.json';
 
-export const RECIPES = [TomatoSoup, PadThai];
+export const RECIPES = [TomatoSoup, PadThai, LemonZucchiniPasta];
